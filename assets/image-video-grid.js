c9ff4229be7f7cab2b96cab0f5412e4c4231447d
GPAR <42031050+Creatpix111@users.noreply.github.com>
2024-09-25 17:22:31 +0530

    see image video grid section

--- assets/image-video-grid.js
+++ assets/image-video-grid.js
@@ -1,9 +1,31 @@
 function videoToggle(videoId) {
-  var videoElement = document.getElementById(videoId);
-  videoElement.style.width = '100%';
-  videoElement
-    ? videoElement.paused
-      ? videoElement.play()
-      : videoElement.pause()
-    : console.error('Video element not found for id:', videoId);
+  const originalVideo = document.getElementById(videoId);
+
+  const fullscreenContainer = document.getElementById('video-fullscreen');
+  const fullscreenVideo = document.getElementById('fullscreen-video');
+
+  fullscreenVideo.src = originalVideo.querySelector('source').src;
+  fullscreenVideo.poster = originalVideo.poster;
+
+  fullscreenContainer.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+
+  fullscreenVideo.play();
 }
+
+function closeFullscreen() {
+  const fullscreenContainer = document.getElementById('video-fullscreen');
+  const fullscreenVideo = document.getElementById('fullscreen-video');
+
+  fullscreenVideo.pause();
+  fullscreenVideo.currentTime = 0;
+
+  fullscreenContainer.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+document.getElementById('video-fullscreen').addEventListener('click', function (event) {
+  var videoContener = document.querySelector('.video-contener');
+  if (!videoContener.contains(event.target)) {
+    closeFullscreen();
+  }
+});
