6d1c012fb47b571192adf1346b6929aafb8503ed
GPS <>
2024-09-30 14:52:42 +0530

    WIP offer deal section

---
+++ assets/offer-swiper.js
@@ -0,0 +1,15 @@
+console.log('...<<<<<<<ddkddd>>>>>>>>>>>>>>');
+
+var swiper = new Swiper('.collections-swiper', {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+console.log('hhgihgig');
